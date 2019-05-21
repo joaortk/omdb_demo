b1223b266c5978653faa2b1993de59418c509a60
joao.rutkoski <joao.rutkoski@agibank.com.br>
2019-05-21 07:36:03 -0300

    Movie List Screen

---
+++ app/src/main/java/br/com/demo/omdbdemo/feature/list/view/MovieListActivity.kt
@@ -0,0 +1,14 @@
+package br.com.demo.omdbdemo.feature.list.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.demo.omdbdemo.R
+
+class MovieListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_list)
+    }
+
+}
