64113a8d43ccb0b36a873e5a083fd8c68d5579c7
joao.rutkoski <joao.rutkoski@agibank.com.br>
2019-05-22 13:05:35 -0300

    MovieDetail Screen

--- app/src/main/java/br/com/demo/omdbdemo/common/provider/ResourceProviderImpl.kt
+++ app/src/main/java/br/com/demo/omdbdemo/common/provider/ResourceProviderImpl.kt
@@ -1,9 +1,8 @@
 package br.com.demo.omdbdemo.common.provider
 
 import android.content.Context
-import javax.inject.Inject
 
-class ResourceProviderImpl @Inject constructor(private val context: Context) : ResourceProvider {
+class ResourceProviderImpl(private val context: Context) : ResourceProvider {
     override fun getString(resId: Int): String {
         return context.getString(resId)
     }
