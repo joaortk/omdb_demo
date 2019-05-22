567ac19636d21ea282d956b78461e1997bf4ac85
Cleber <cleber.neto@ilegra.com>
2019-05-22 00:13:42 -0300

    Configuring styles and adding search view

--- app/src/main/java/br/com/demo/omdbdemo/MainActivity.kt
+++ app/src/main/java/br/com/demo/omdbdemo/MainActivity.kt
@@ -2,8 +2,13 @@ package br.com.demo.omdbdemo
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
 import javax.inject.Inject
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +18,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val myToolbar = findViewById<Toolbar>(R.id.main_toolbar)
+        setSupportActionBar(myToolbar)
         OmdbDemoApplication.appComponent.inject(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_menu, menu)
+        (menu?.findItem(R.id.action_search)?.actionView as? SearchView)?.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Log.d("SearchView", "OnSubmit $query")
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                Log.d("SearchView", "OnChange $newText")
+                return true
+            }
+        })
+        return true
     }
 }
