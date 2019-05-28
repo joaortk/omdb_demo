f7518c5394421e082849cc6cfdb832625c6039b2
joao.rutkoski <joao.rutkoski@agibank.com.br>
2019-05-28 08:21:52 -0300

    Meetup - Live Coding Start

--- app/src/main/java/br/com/demo/omdbdemo/common/bindingadapter/ImageViewAdapter.kt
+++ app/src/main/java/br/com/demo/omdbdemo/common/bindingadapter/ImageViewAdapter.kt
@@ -1,14 +1,1 @@
 package br.com.demo.omdbdemo.common.bindingadapter
-
-import android.widget.ImageView
-import androidx.databinding.BindingAdapter
-import com.squareup.picasso.Picasso
-
-@BindingAdapter("android:src")
-fun setImageUrl(imageView: ImageView, url: String?) {
-    url?.run {
-        if (url.isNotEmpty()) {
-            Picasso.get().load(this).into(imageView)
-        }
-    }
-}
